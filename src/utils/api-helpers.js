18587f4bcd441cbe4b125568075e482ca5bcfb5d
JaxomofRuatha <jchritton33@gmail.com>
2017-10-20 23:54:28 -0500

    Setting up front end template

---
+++ src/utils/api-helpers.js
@@ -0,0 +1,50 @@
+const REQUEST_TIMEOUT_MS = 30000;
+
+const _checkStatus = (res) => {
+  if (res.ok) {
+    return res[res.status === 204 ? 'text' : 'json']();
+  } else if (res.status === 401) {
+    throw new Error('Unauthorized');
+  } else {
+    const error = new Error(res.statusText);
+    error.response = res;
+    throw error;
+  }
+};
+
+function timedRequest(ms, promise) {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject(new Error('Request Timeout'));
+    }, ms);
+    promise.then(
+      (res) => {
+        clearTimeout(timeoutId);
+        resolve(res);
+      },
+      (err) => {
+        clearTimeout(timeoutId);
+        reject(err);
+      }
+    );
+  });
+}
+
+function apiSkeleton(url, options, onRequestSuccess, onRequestFail) {
+  if (!url) onRequestFail(new Error('Request url is a required field'));
+  if (!options) onRequestFail(new Error('Request options is a required field'));
+  const reqOptions = {
+    mode: 'cors',
+    ...options
+  };
+  timedRequest(REQUEST_TIMEOUT_MS, fetch(url, reqOptions))
+    .then(_checkStatus)
+    .then((res) => {
+      onRequestSuccess(res);
+    })
+    .catch((error) => {
+      onRequestFail(error);
+    });
+}
+
+export default apiSkeleton;
