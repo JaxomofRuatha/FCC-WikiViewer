ccc3f7a5885e4e925a5659d74977c0a1f1566f8c
John Chritton <jchritton33@gmail.com>
2018-02-06 20:05:57 -0600

    Initial styling added

--- src/components/ArticleEntry.js
+++ src/components/ArticleEntry.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import styled from 'react-emotion';
+import color from 'color';
+
+const darkBrown = color('#484538')
+  .darken(0.4)
+  .hex();
 
 const ArticleEntry = props => (
   <article className={props.className}>
     <a href={props.url}>
+      <div className="article-text">
+        <h1>{props.title}</h1>
+        <p>{props.extract}</p>
+      </div>
       <img src={props.thumbnail} alt={props.caption} />
-      <h1>{props.title}</h1>
-      <p>{props.extract}</p>
     </a>
   </article>
 );
 
 const ArticleStyled = styled(ArticleEntry)`
-  background: #d4eac8;
-  color: #484538;
+  height: 15rem;
+  margin: 2rem;
+  font-family: sans-serif;
+  * {
+    box-sizing: border-box;
+  }
   a {
     text-decoration: none;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    color: #c0d8e0;
+    background: ${darkBrown};
+    padding: 1rem;
+    border: 3px solid ${darkBrown};
+    border-radius: 10px;
+    transition: all 0.3s;
+    &:hover {
+      border: 3px solid #56a3a6;
+    }
+  }
+  .article-text {
+    padding-right: 1rem;
+  }
+  h1 {
+    color: #cad49d;
+  }
+  img {
+    width: 30%;
+    height: auto;
   }
 `;
 
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form/immutable';
 import styled from 'react-emotion';
+import color from 'color';
+
+const lightBrown = color('#484538')
+  .lighten(0.6)
+  .hex();
 
 const SearchBox = ({
   requestArticles,
@@ -9,8 +14,11 @@ const SearchBox = ({
   className
 }) => (
   <div className={`search-box ${className}`}>
-    <img src="assets/Wikipedia-logo-v2.svg" alt="The logo of Wikipedia" />
-    <form className="search-form" onSubmit={handleSubmit}>
+    <header className="search-box__title">
+      <img src="assets/Wikipedia-logo-v2.svg" alt="The logo of Wikipedia" />
+      <h1>The WikiViewer</h1>
+    </header>
+    <form className="search-box__form" onSubmit={handleSubmit}>
       <Field
         name="searchInput"
         component="input"
@@ -18,30 +26,78 @@ const SearchBox = ({
         placeholder="Consult Wikipedia's knowledge..."
         required
       />
-      <button type="submit" disabled={submitting}>
-        Search!
+      <button
+        className="search-box__button"
+        type="submit"
+        disabled={submitting}
+      >
+        GO
       </button>
     </form>
-    <a href="https://en.wikipedia.org/wiki/Special:Random">
+    <a
+      href="https://en.wikipedia.org/wiki/Special:Random"
+      className="search-box__random"
+    >
       ...or discover something new!
     </a>
   </div>
 );
 
 const SearchStyled = styled(SearchBox)`
-  width: 40%;
-  margin: 0 auto;
-  display: flex;
+  width: 50vw;
+  margin: 6rem auto;
+  .search-box__title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  h1 {
+    color: #56a3a6;
+    font-family: 'Linux Libertine';
+    font-weight: 100;
+    font-variant: small-caps;
+    font-size: 3rem;
+    display: inline-block;
+    padding: 0 1.5rem;
+  }
+  form {
+    position: relative;
+    margin: 1rem 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
   a {
-    text-decoration: none;
+    text-align: center;
+    color: #c0d8e0;
+    font-family: sans-serif;
+    padding-left: 2rem;
   }
   input {
-    background: #c0d8e0;
-    flex: 5;
+    background: ${lightBrown};
+    border: none;
     padding: 1em;
+    color: #c0d8e0;
+    font-weight: bold;
+    width: 80%;
+    border-radius: 5px;
+    &:required {
+      box-shadow: none;
+    }
   }
   button {
-    flex: 1;
+    text-align: center;
+    display: inline-block;
+    width: 3rem;
+    height: 3rem;
+    font-size: 1rem;
+    border-radius: 50%;
+    font-weight: bold;
+    background: #484538;
+    border: 2px solid #484538;
+    box-shadow: inset 0 0 0 3px #56a3a6, 0 0 0 3px ${lightBrown};
+    color: #56a3a6;
+    margin-left: -1rem;
   }
 `;
 
